c1c0a4d0214869d030a19deea50efcc9f06b4ed9
jaijymartinpaul <jaijymartinpaul@gmail.com>
2022-07-12 12:39:29 +0000

    Third commit: Production server created

--- main.tf
+++ main.tf
@@ -6,30 +6,38 @@ variable "project_env"{
     default = "prod"
 }
 
+variable "instance_type"{
+    default = "t2.micro"
+}
+
+variable "instance_ami"{
+    default = "ami-08df646e18b182346"
+}
+
 resource "aws_security_group" "webserver" {
-      
+
   name        = "webaccess-${var.project_name}-${var.project_env}"
   description = "Allow 80,443,22 traffic"
-  
-    
+
+
   ingress {
-   
+
     from_port        = 443
     to_port          = 443
     protocol         = "tcp"
     cidr_blocks      = ["0.0.0.0/0"]
     ipv6_cidr_blocks = ["::/0"]
   }
-    
+
   ingress {
-   
+
     from_port        = 80
     to_port          = 80
     protocol         = "tcp"
     cidr_blocks      = ["0.0.0.0/0"]
     ipv6_cidr_blocks = ["::/0"]
   }
-    
+
 
   egress {
     from_port        = 0
@@ -40,11 +48,31 @@ resource "aws_security_group" "webserver" {
   }
 
   tags = {
+
+       Name = "webserver-${var.project_name}-${var.project_env}"
+       project = var.project_name
+       env = var.project_env
+  }
+
+}
+
+# --------------------------------------------------------------
+# Creating Ec2 Instance
+# --------------------------------------------------------------
+
+resource "aws_instance"  "webserver" {
+    
+    ami                    =  var.instance_ami
+    instance_type          =  var.instance_type
+    key_name               =  "devops-class"
+    vpc_security_group_ids =  [ aws_security_group.webserver.id ]
+    user_data              = file("setup.sh")
+
+    tags = {
       
        Name = "webserver-${var.project_name}-${var.project_env}"
        project = var.project_name
        env = var.project_env
   }
-    
 }
 
